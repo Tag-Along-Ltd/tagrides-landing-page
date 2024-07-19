23dc0381884444f946e50ab155f58e8acbca9b23
Prosper Atu <atu.prosper@gmail.com>
2024-07-19 21:33:44 +0100

    Updated the menu style 1

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,19 @@
+import LayoutStyle7 from '@/components/Layouts/LayoutStyle7';
+import Blog3ColumnContent from '@/components/blog/Blog3ColumnContent';
+import React from 'react';
+
+export const metadata = {
+    title: "Tag Rides - Ride-Sharing Service -   Blog 3 Column"
+}
+
+const Blog = () => {
+    return (
+        <>
+            <LayoutStyle7 breadCrumb="blog-3-column" title="Blog 3 Column">
+                <Blog3ColumnContent />
+            </LayoutStyle7>
+        </>
+    );
+};
+
+export default Blog;
