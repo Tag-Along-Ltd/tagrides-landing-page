1219fe4ff21bb9cb9d9e9e314077838b314697ba
Prosper Atu <atu.prosper@gmail.com>
2024-07-20 12:29:39 +0100

    updated fav

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -8,23 +8,26 @@ const Loading = () => {
                     <div className="animation-preloader">
                         <div className="spinner"></div>
                         <div className="txt-loading">
-                            <span data-text-preloader="C" className="letters-loading">
-                                C
-                            </span>
-                            <span data-text-preloader="O" className="letters-loading">
-                                O
-                            </span>
-                            <span data-text-preloader="N" className="letters-loading">
-                                N
-                            </span>
-                            <span data-text-preloader="S" className="letters-loading">
-                                S
-                            </span>
-                            <span data-text-preloader="U" className="letters-loading">
-                                U
+                            <span data-text-preloader="T" className="letters-loading">
+                                T
                             </span>
                             <span data-text-preloader="A" className="letters-loading">
                                 A
+                            </span>
+                            <span data-text-preloader="G" className="letters-loading">
+                                G
+                            </span>
+                            <span data-text-preloader="R" className="letters-loading">
+                                R
+                            </span>
+                            <span data-text-preloader="I" className="letters-loading">
+                                I
+                            </span>
+                            <span data-text-preloader="D" className="letters-loading">
+                                D
+                            </span>
+                            <span data-text-preloader="E" className="letters-loading">
+                                E
                             </span>
                         </div>
                     </div>
